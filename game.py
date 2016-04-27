dd903e42e79a615812b62d7cc94927bf5ab22f40
ndevox <nick.a.sarbicki@gmail.com>
2016-04-27 20:21:15 +0100

    adding main func

--- game.py
+++ game.py
@@ -1,29 +1,35 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((800, 640))
-done = False
-x = 400
-y = 320
+def main():
 
-clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((800, 640))
+    done = False
+    x = 400
+    y = 320
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    clock = pygame.time.Clock()
 
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] or pressed[pygame.K_w]: y -= 3
-    if pressed[pygame.K_DOWN] or pressed[pygame.K_s]: y += 3
-    if pressed[pygame.K_LEFT] or pressed[pygame.K_a]: x -= 3
-    if pressed[pygame.K_RIGHT] or pressed[pygame.K_d]: x += 3
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-    screen.fill((0, 0, 0))
-    color = (255, 100, 0)
-    pygame.draw.polygon(screen, color, [[105,105],[205,205],[255,255],[105,255]],1)
-    pygame.draw.polygon(screen, color, [[400,400],[500,400],[500,500],[400,500]],1)
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 5, 5))
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP] or pressed[pygame.K_w]: y -= 3
+        if pressed[pygame.K_DOWN] or pressed[pygame.K_s]: y += 3
+        if pressed[pygame.K_LEFT] or pressed[pygame.K_a]: x -= 3
+        if pressed[pygame.K_RIGHT] or pressed[pygame.K_d]: x += 3
 
-    pygame.display.flip()
-    clock.tick(60)
+        screen.fill((0, 0, 0))
+        color = (255, 100, 0)
+        pygame.draw.polygon(screen, color, [[105,105],[205,205],[255,255],[105,255]],1)
+        pygame.draw.polygon(screen, color, [[400,400],[500,400],[500,500],[400,500]],1)
+        pygame.draw.rect(screen, color, pygame.Rect(x, y, 5, 5))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    main()
