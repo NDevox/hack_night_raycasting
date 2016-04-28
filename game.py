bdf918058e3328bc7789cf32dcaf98c118b5009c
Nick Sarbicki <nick.a.sarbicki@gmail.com>
2016-04-28 13:50:58 +0100

    comparing to y=mx+c

--- game.py
+++ game.py
@@ -3,11 +3,23 @@ import math
 from polywog import Polywog
 
 
+def get_x_y(pos1, pos2):
+    """
+    Get the difference between two co-ordinates.
+
+    :param pos1: tuple, co-ordinate 1.
+    :param pos2: tuple, co-ordinate 2.
+    :return: tuple, co-ordinate difference.
+    """
+    return pos1[0] - pos2[0], pos1[1] - pos2[1]
+
+
 def which_side(x, y):
     """
     We need to figure out which quadrant the polygon is in relative to our point.
-    :param x: int, x co-ord difference
-    :param y: int, y co-ord difference
+
+    :param x: int, x co-ord difference.
+    :param y: int, y co-ord difference.
     :return: dict, containing if the polygon is left and below the point.
     """
     pos = {}
@@ -19,10 +31,10 @@ def which_side(x, y):
     else:  # I believe a false here avoids a DivisionByZero error.
         pos['left'] = False
 
-    if y < 0:  # it is not below
+    if y < 0:  # it is below
         pos['bottom'] = False
     elif y > 0:
-        pos['bottom'] = False
+        pos['bottom'] = True
     else:  # avoiding DivisionByZero
         pos['bottom'] = False
 
@@ -39,19 +51,35 @@ def workout_angles(person, shape):
     """
     angles = []
     for point in shape:
-        x, y = person[0] - point[0], person[1] - point[1]
+        x, y = get_x_y(person, point)
         pos = which_side(x, y)
+        try:
+            if (pos['left'] and not pos['bottom']) \
+                or not pos['left'] and pos['bottom']:
+                angles.append((point, math.atan(y/x), pos))
 
-        if (pos['left'] and not pos['bottom']) \
-            or not pos['left'] and pos['bottom']:
-            angles.append((point, math.atan(y/x), pos))
-
-        else:
-            angles.append((point, math.atan(x/y), pos))
+            else:
+                angles.append((point, math.atan(x/y), pos))
+        except ZeroDivisionError:
+            pass
 
     angles.sort(key=lambda x: x[1])
 
     return angles[0], angles[-1]
+
+
+def angle_y(angle, pos, corner):
+    """
+    Is the angle between the corner to the polygon bigger than the polygon to the person?
+
+    :param angle: float, Angle between person and polygon.
+    :param pos: tuple(x,y), Polygon position.
+    :param corner: tuple(x,y), corner.
+    :return: bool, it is or isn't.
+    """
+    x, y = get_x_y(pos, corner)
+
+    return math.atan(x/y) > angle
 
 
 def create_polygon(person, shape, size=(0, 0)):
@@ -67,22 +95,50 @@ def create_polygon(person, shape, size=(0, 0)):
 
     points = []
 
-    # I think we all now know that I am not good at trig under pressure.
     for angle in angles:
+
+        m = (angle[0][1] - person[1])/(angle[0][0]-person[0])
+        c = (m*person[0] - person[1])
+        # They are in the top left.
         if angle[2]['left'] and not angle[2]['bottom']:
-            points.append((person[1] - round(person[0] * math.tan(angle[1])), 0))
+            if angle_y(angle[1], angle[0], (0,0)):
+                y = person[1] - round(person[0] * math.tan(angle[1]))
+                points.append((0, y))
+            else:
+                x = person[0] - round(person[1] / math.tan(angle[1]))
+                points.append((x, 0))
 
+        # They are in the top right.
         elif not angle[2]['left'] and not angle[2]['bottom']:
-            points.append((person[0] - round((size[1] - person[1]) * math.tan(angle[1])), 0))
+            if angle_y(angle[1], angle[0], (size[0],0)):
+                print(1)
+                y = round(m * size[0] + c)
+                points.append((size[0], y))
+            else:
+                print(2)
+                x = round((0 + c) / m)
+                points.append((x, 0))
 
+        # They are in the bottom right.
         elif not angle[2]['left'] and angle[2]['bottom']:
-            points.append((size[0], person[0] - round((size[1] - person[1]) * math.tan(angle[1]))))
+            if angle_y(angle[1], angle[0], size):
+                y = person[1] + round(person[0] * math.tan(angle[1]))
+                points.append((size[0], y))
+            else:
+                x = person[0] + round(person[1] / math.tan(angle[1]))
+                points.append((x, size[1]))
 
+        # They are in the bottom left.
         elif angle[2]['left'] and angle[2]['bottom']:
-            points.append((size[0], person[1] - round((person[0] * math.tan(angle[1])))))
+            if angle_y(angle[1], angle[0], (0,size[1])):
+                y = round(m * 0 + c)
+                points.append((0, y))
+            else:
+                x = round((size[1] + c) / m)
+                points.append((x, size[1]))
 
     # I'll think of why this logically works soon.
-    return sorted(points + [angles[0][0], angles[1][0]], key=lambda x:min(map(abs, x)))
+    return sorted(points + [angles[0][0], angles[1][0]], key=lambda x:max(map(abs, x)))
 
 
 def main():
@@ -95,7 +151,6 @@ def main():
     player = Polywog(color)
     clock = pygame.time.Clock()
     block_list = pygame.sprite.Group()
-    block_list.add(player)
 
     while not done:
         for event in pygame.event.get():
@@ -111,8 +166,6 @@ def main():
 
         screen.fill((0, 0, 0))
 
-
-
         player.rect.x = x
         player.rect.y = y
         block = Polywog(color)
@@ -126,13 +179,15 @@ def main():
         block_list.add(block2)
         block_list.draw(screen)
 
-        pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, block.corners())[0][0], player.rect.center], 1)
-        pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, block.corners())[1][0], player.rect.center], 1)
-        pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, block2.corners())[0][0], player.rect.center], 1)
-        pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, block2.corners())[1][0], player.rect.center], 1)
-        pygame.draw.polygon(screen, color, create_polygon(player.rect.center, block.corners(), (800, 640)), 0)
-        pygame.draw.polygon(screen, color, create_polygon(player.rect.center, block2.corners(), (800, 640)), 0)
-        pygame.display.flip()
+        for obj in block_list:
+            # draw lines of sight to the corners.
+            pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, obj.corners())[0][0], player.rect.center], 1)
+            pygame.draw.lines(screen, color, False, [workout_angles(player.rect.center, obj.corners())[1][0], player.rect.center], 1)
+
+            # Draw the shadows.
+            pygame.draw.polygon(screen, color, create_polygon(player.rect.center, obj.corners(), (800, 640)), 0)
+
+        pygame.display.update()
         clock.tick(60)
 
 
